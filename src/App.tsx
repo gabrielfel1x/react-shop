06ba1e27c3bd459d965a109bc0c01fcf28d4c621
gabrielfel1x <gabrielfelxx@gmail.com>
2024-06-05 13:40:50 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Header from "./containers/header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/productListing";
+import ProductDetail from "./containers/productDetail";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<div>404 Not Found!</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
